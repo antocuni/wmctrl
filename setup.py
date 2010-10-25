ec186343d4ba678ee88371fb830554f5f0da47ad
antocuni <antocuni@localhost>
2010-10-26 00:07:59 +0200

    add a basic setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='wmctrl',
+    version='0.1',
+    author='Antonio Cuni',
+    author_email='anto.cuni@gmail.com',
+    py_modules=['wmctrl'],
+    url='http://bitbucket.org/antocuni/wmctrl',
+    license='BSD',
+    description='A tool to programmatically control windows inside X',
+    long_description='A tool to programmatically control windows inside X'
+)
