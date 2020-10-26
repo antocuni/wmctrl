649a0e1446b0f490a8dce7cffa6f2685f3b23583
John Vandenberg <jayvdb@gmail.com>
2020-10-26 11:32:49 +0700

    setup.py: Update URL

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='Antonio Cuni',
     author_email='anto.cuni@gmail.com',
     py_modules=['wmctrl'],
-    url='http://bitbucket.org/antocuni/wmctrl',
+    url='https://github.com/antocuni/wmctrl',
     license='MIT',
     description='A tool to programmatically control windows inside X',
     long_description='A tool to programmatically control windows inside X'
