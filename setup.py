91a2cdc3b1ab8e9800dc61bd89891aba4d964d93
Antonio Cuni <anto.cuni@gmail.com>
2021-04-23 18:38:22 +0200

    new version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='wmctrl',
-    version='0.3',
+    version='0.4',
     author='Antonio Cuni',
     author_email='anto.cuni@gmail.com',
     py_modules=['wmctrl'],
